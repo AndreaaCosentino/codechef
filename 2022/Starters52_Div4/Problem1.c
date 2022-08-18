e00f8e9511f7c0e4ca24ff939a46a9e1c5cffbed
AndreaaCosentino <110279497+AndreaaCosentino@users.noreply.github.com>
2022-08-18 11:15:23 +0200

    Create Problem1.c

---
+++ 2022/Starters52_Div4/Problem1.c
@@ -0,0 +1,14 @@
+/*Problem text: https://www.codechef.com/submit/AIANALYSE?tab=statement*/
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	if(n <= 1000)
+	{
+	    printf("Yes");
+	}else{printf("NO");}
+	return 0;
+}
+
